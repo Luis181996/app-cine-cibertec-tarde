ceb89e1ceaf8da711c509983d04aedae322cbf32
Luis181996 <luishumberto181996.com>
2023-10-22 16:46:06 -0500

    ejercicio terminado

---
+++ src/main/java/pe/edu/cibertec/appcinecibertectarde/service/EstadoService.java
@@ -0,0 +1,28 @@
+package pe.edu.cibertec.appcinecibertectarde.service;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import pe.edu.cibertec.appcinecibertectarde.model.bd.Estado;
+import pe.edu.cibertec.appcinecibertectarde.repository.EstadoRepository;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class EstadoService {
+
+    private EstadoRepository estadoRepository;
+
+    public List<Estado> listadoEstados(){
+        return estadoRepository.findAll();
+    }
+
+    public boolean registrarEstado(Estado estado){
+        return estadoRepository.save(estado) !=null;
+    }
+
+    public void eliminarEstado(Integer idestado){
+        estadoRepository.deleteById(idestado);
+    }
+
+}
